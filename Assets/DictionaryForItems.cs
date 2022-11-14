759e35634c86d3e132845a60c9036fed243e5b99
AnuragNegii <negianurag99@gmail.com>
2022-11-14 12:07:26 +0530

    foreach loop for dictionaries

--- Assets/DictionaryForItems.cs
+++ Assets/DictionaryForItems.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class DictionaryForItems : MonoBehaviour
 {
     Dictionary<string, int> ItemInventory = new Dictionary<string, int>()
-    {   {"Potions", 5},
-        {"Antidode", 8},
+    {   {"Potions", 5}, //{key, value}
+        {"Antidode", 7},
         { "Aspirin", 1}
     };
     // Start is called before the first frame update
     void Start()
     {
         Debug.LogFormat("Items : {0 }", ItemInventory.Count);
+    foreach(KeyValuePair<string, int> kvp in ItemInventory)
+    {
+        Debug.LogFormat("Item {0}: - {1}g", kvp.Key, kvp.Value);
     }
+    }
+
 
     // Update is called once per frame
     void Update()
